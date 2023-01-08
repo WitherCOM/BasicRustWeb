459a72ab32a700bb1389b55886b971436a712199
Németh Csaba <nemeth.csaba.tibor@gmail.com>
2023-01-08 14:25:46 +0100

    Fixed typo

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn index() -> &'static string {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route('/',get(index));
+    let app = Router::new().route("/",get(index));
         
     axum::Server::bind(&"0.0.0.0:80".parse().unwrap())
         .serve(app.into_make_service())
